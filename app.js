de5f263558179e413ddfe1a55d7e6471f87f1b27
Constanza Delfino <conidelfino@gmail.com>
2022-10-28 16:35:53 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+let boton=document.querySelector('#boton')
+
+boton.addEventListener('click', ()=>{
+    document.body.classList.toggle('modoOscuro')
+})
+
+//toggle: me activa o desactiva una clase
